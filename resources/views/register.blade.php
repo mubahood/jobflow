f49da87a7250330503f106001c3c92fc3d2179d9
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-04-16 20:43:27 +0300

    F. Musenene 🥰

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -69,7 +69,7 @@
                             <p class="nav d-block  text-center text-md-start pb-2 pb-lg-0 mb-0">
                                 Hand-made with ❤️ by
                                 <a class="nav-link d-inline-block p-0" href="https://twitter.com/8TechConsults"
-                                    target="_blank" rel="noopener">8Technologies 🥰</a>
+                                    target="_blank" rel="noopener">F. Musenene 🥰</a>
                             </p>
                         </div>
                     </div>
